65f2a251566ba6d0b5bc1ae5b973859db6f6ccbd
Bhupendrxsingh <89328517+oamohm@users.noreply.github.com>
2026-06-11 03:10:17 +0530

    Update dependencies in layout.js
    Updated dependencies in layout.js for Next.js and React.

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+"dependencies": {
+  "next": "14.0.0",
+  "react": "^18",
+  "react-dom": "^18",
+  "wagmi": "^1.4.0",
+  "ethers": "^6.0.0"
 }
